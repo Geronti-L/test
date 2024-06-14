269b27a0f69015b493f5476b9c1729dade37c9e8
nikaaraba777 <nikaaraba777@yahoo.com>
2024-06-14 19:35:14 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Geronti Liparteliani on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
